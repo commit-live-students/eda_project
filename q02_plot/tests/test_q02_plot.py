27c83ec7f8ed663bcd2817656ec0ee3957f7b1db
gyandhanee <gyanendra.dhanee@gmail.com>
2017-10-04 01:00:36 +0530

    delete tests

--- q02_plot/tests/test_q02_plot.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-from q02_plot.build import plot
-
-class TestLoad_distplot(TestCase):
-    def test_plot(self):
-        plot(['LotArea', 'GarageArea', 'OpenPorchSF', 'SalePrice'])
-        self.assertTrue(True)
--- q03_regression_plot/tests/test_q03_regression_plot.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-from q03_regression_plot.build import regression_plot
-
-class TestLoad_regression_plot(TestCase):
-    def test_regression_plot(self):
-        regression_plot('GrLivArea', 'SalePrice')
-        self.assertTrue(True)
