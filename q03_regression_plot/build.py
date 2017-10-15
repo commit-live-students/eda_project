29f51dbafcea1ac68c680e75acb024c1f184a1c8
msonumanish <msonu.manish@gmail.com>
2017-10-15 20:15:33 +0000

    Done

--- q03_regression_plot/build.py
+++ q03_regression_plot/build.py
@@ -1,15 +1,27 @@
+# %load q03_regression_plot/build.py
 # Default imports
 
 import pandas as pd
 import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
-
+from sklearn.linear_model import LinearRegression
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
+X = data.LotArea[:,np.newaxis]  # Reshape
+y = data.SalePrice
 # Write your code here
+def regression_plot(X,y):
+    regressor = LinearRegression()
+    regressor.fit(X, y)
+    y_pred = regressor.predict(X)
+    plt.scatter(X, y)
+    plt.plot(X, y_pred, "r-")
+    plt.title('Housing Price ')
+    plt.xlabel('Area')
+    plt.ylabel('Price')
+    plt.show()
 
-
-
-
+#regression_plot(X,y)
