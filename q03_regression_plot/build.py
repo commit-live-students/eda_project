5ff99a3fd44d9ec3e3cbf992f51e695a25e5641c
sudharsanasai <Sudharsanasai@gmail.com>
2017-10-15 16:35:58 +0000

    Done

--- q03_regression_plot/build.py
+++ q03_regression_plot/build.py
@@ -7,9 +7,7 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
-
+def regression_plot(f1,f2):
+    sns.regplot(f1,f2,data=data)
+    plt.show()
 # Write your code here
-
-
-
-
