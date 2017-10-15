cf6d6ed141c7534f7ac3aec7df1621bb669ae03b
msonumanish <msonu.manish@gmail.com>
2017-10-15 20:24:08 +0000

    Done

--- q04_cor/build.py
+++ q04_cor/build.py
@@ -1,3 +1,4 @@
+# %load q04_cor/build.py
 # Default imports
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,5 +8,14 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here
+def cor(data):
+    plt.figure(figsize=(12,8))
+    #sns.heatmap(data.corr(), cmap='viridis')
+    k = 10 #number of variables for heatmap
+    cols = data.corr().nlargest(k, 'SalePrice')['SalePrice'].index
+    cm = data[cols].corr()
+    plt.figure(figsize=(10,6))
+    sns.heatmap(cm, annot=True, cmap = 'viridis')
+    plt.show()
 
-
+#cor(data)
