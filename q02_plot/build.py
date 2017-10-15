e09a8cdeb41f5668a94eb8ff873873d18d675000
apoorvabanubakode <apoorva.banubakode@gmail.com>
2017-10-15 10:33:13 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -7,5 +7,21 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here:
+def plot(num_cols):
+
+    facet = None
+    i=0
+    plt.figure(figsize=(10,4))
+    plt.subplot(221)
+    sns.boxplot(facet, num_cols[i],data = data)
+    plt.subplot(222)
+    sns.boxplot(facet, num_cols[i+1],data = data)
+    plt.subplot(223)
+    sns.boxplot(facet, num_cols[i+2],data = data)
+    plt.subplot(224)
+    sns.boxplot(facet, num_cols[i+3],data = data)
 
 
+    plt.show()
+    return plt
+    
