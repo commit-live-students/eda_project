49fa170e60e55460cfc32c6456c614a26efcf316
Mohammed-Sunasra <sunasramohd@gmail.com>
2017-10-24 10:42:55 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -2,7 +2,21 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import matplotlib
+matplotlib.pyplot.switch_backend('agg')
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 # Write your code here:
+def plot(num_cols):
+    plt.subplot(2,2,1)
+    plt.hist(data['LotArea'])
+    plt.subplot(2,2,2)
+    plt.hist(data['GarageArea'])
+    plt.subplot(2,2,3)
+    plt.hist(data['OpenPorchSF'])
+    plt.subplot(2,2,4)
+    plt.hist(data['SalePrice'])
+    plt.show()
+    
+plot(len(data.columns))
