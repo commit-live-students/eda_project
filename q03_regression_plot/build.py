cefd272bb5225ab8d4a1fb4c564db2c73a80ffca
Mohammed-Sunasra <sunasramohd@gmail.com>
2017-10-24 10:53:38 +0000

    Done

--- q03_regression_plot/build.py
+++ q03_regression_plot/build.py
@@ -3,13 +3,16 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.pyplot.switch_backend('agg')
 
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here
+def regression_plot(x,y):
+    plt.scatter(x,y)
+    plt.show()
 
-
-
-
+regression_plot(data['GrLivArea'],data['SalePrice'])
