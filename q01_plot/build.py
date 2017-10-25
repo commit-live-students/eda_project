e65da0e145414c3bd2c34e939e7dc08c1f2b92fd
AnujaySaraf <anujaysaraf7@gmail.com>
2017-10-25 18:53:27 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -1,4 +1,6 @@
 # Default imports
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -6,3 +8,10 @@ import pandas as pd
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 # Write your code here:
+def plot(num_cols):
+    global data
+
+    for i, col in enumerate(num_cols):
+        plt.subplot(len(num_cols), 1 ,i+1)
+        plt.hist(data[col])
+    plt.show()
