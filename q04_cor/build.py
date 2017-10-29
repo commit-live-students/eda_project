133e09e86ba430e45d51fef62c949ce9e74c0c80
Sakshi-BUK <sakshi.dhokrat96@gmail.com>
2017-10-29 16:14:11 +0000

    Done

--- q04_cor/build.py
+++ q04_cor/build.py
@@ -1,3 +1,4 @@
+# %load q04_cor/build.py
 # Default imports
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,7 +6,9 @@ import seaborn as sns
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
-
+def cor(data):
+    return data[data.columns[1:]].corr()
 # Write your code here
 
 
+#cor(data)
