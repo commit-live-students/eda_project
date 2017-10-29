0908e376ea7e0e4bda861e589b03d4e683c4fd8d
sandiprb <baradiya.sandip@gmail.com>
2017-10-29 14:40:22 +0000

    Done

--- q03_regression_plot/build.py
+++ q03_regression_plot/build.py
@@ -8,8 +8,6 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
-# Write your code here
-
-
-
-
+def regression_plot(x,y):
+    plt.scatter(x=x, y=y)
+    plt.show()
