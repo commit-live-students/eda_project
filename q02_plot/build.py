e69f85feda1fefeb1857cd089905a5ffce438391
sandiprb <baradiya.sandip@gmail.com>
2017-10-29 14:36:22 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -4,3 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
+
+
+def plot(data):
+    data.boxplot(['LotArea', 'GarageArea', 'OpenPorchSF', 'SalePrice'])
+    plt.show()
