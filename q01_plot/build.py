cd4f00319c55b1b1035dcdd04931b16b0af68031
sandiprb <baradiya.sandip@gmail.com>
2017-10-29 14:32:48 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -6,3 +6,6 @@ import pandas as pd
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 # Write your code here:
+def plot(data):
+    data.hist(['LotArea', 'GarageArea','OpenPorchSF','SalePrice'])
+    plt.show()
