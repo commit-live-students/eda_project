79a1f38d26180359d57c3f282141fa2308830e4e
vinod-boga <vinodboga17@gmail.com>
2017-11-02 04:04:03 +0000

    Done

--- q04_cor/build.py
+++ q04_cor/build.py
@@ -1,11 +1,18 @@
 # Default imports
-import pandas as pd
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+import numpy as np
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here
-
-
+def cor(data):
+    fig, ax = plt.subplots(figsize=(10,10))
+    corr = data.corr()
+    sns.heatmap(corr,ax=ax)
+    plt.show()
+cor(data)
