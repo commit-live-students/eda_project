353ac911cd2bc054650b6ca17e771c1718c2792e
SiddharthPant <spsiddharthpant@gmail.com>
2017-11-04 15:43:17 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -7,5 +7,9 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here:
-
-
+def plot(num_cols):
+    fig, axes = plt.subplots(2, 2)
+    fig.set_size_inches(20, 10)
+    for i, ax in enumerate(axes.flatten()):
+        sns.boxplot(x=data.iloc[:, i], orient='h', ax=ax)
+    return fig
