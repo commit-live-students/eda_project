0dddc65e47d99c627a785812c925b4a994c76109
yuvrajkale87 <yuvraj.kale87@gmail.com>
2017-11-09 12:09:15 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -8,4 +8,11 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 # Write your code here:
 
+num_cols = [data['LotArea'], data['GarageArea'], data['OpenPorchSF'], data['SalePrice']]
 
+def plot(num_cols):
+    for n in num_cols:
+        plt.figure(figsize=(10,4))
+        plt.subplot(121)
+        sns.distplot(n, kde=False)
+        plt.show()
