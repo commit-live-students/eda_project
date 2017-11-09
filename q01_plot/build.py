c4e2fb6aaceb8001b4fb0857b6482a3642ee3ac4
yuvrajkale87 <yuvraj.kale87@gmail.com>
2017-11-09 11:40:12 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -6,3 +6,11 @@ import pandas as pd
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 # Write your code here:
+num_cols = [data['LotArea'], data['GarageArea'], data['OpenPorchSF'], data['SalePrice']]
+
+def plot(num_cols):
+    for n in num_cols:
+       # LotArea = data['LotArea']
+        plt.subplot(2,2,1)
+        plt.hist(n)
+        plt.show()
