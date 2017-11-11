0b78fb7de96ae4ffe1b730a91813659a2025ed14
darshind <doshi.darshin@gmail.com>
2017-11-11 17:45:26 +0000

    Done

--- q03_regression_plot/build.py
+++ q03_regression_plot/build.py
@@ -1,15 +1,14 @@
-# Default imports
-
-import pandas as pd
+# %load q03_regression_plot/build.py
+'''Default Import'''
 import seaborn as sns
+import pandas as pd
 import matplotlib.pyplot as plt
-
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
-
 # Write your code here
-
-
-
-
+# Create a function regression_plot(variable1, variable2) that returns a jointplot
+def regression_plot(variable1,variable2):
+    data.plot.scatter(x='GrLivArea',y='SalePrice')
+    plt.show()
+    return
