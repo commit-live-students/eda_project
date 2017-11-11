0f4942265513ee65f2aad6f03ed0bdec1382f804
darshind <doshi.darshin@gmail.com>
2017-11-11 17:51:52 +0000

    Done

--- q04_cor/build.py
+++ q04_cor/build.py
@@ -1,11 +1,16 @@
-# Default imports
+# %load q04_cor/build.py
 import pandas as pd
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
-
 # Write your code here
-
-
+# Create a function cor(dataframe)
+def cor(data):
+    correlation_var = data.corr()
+    sns.heatmap(correlation_var)
+    plt.show()
+    return
