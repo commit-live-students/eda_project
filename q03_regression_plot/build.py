4f25470aa167fc35809ad69cd2bb1805a8716843
jmcode79 <mukherjee.jeet@gmail.com>
2017-11-19 11:36:20 +0000

    Done

--- q03_regression_plot/build.py
+++ q03_regression_plot/build.py
@@ -1,3 +1,4 @@
+# %load q03_regression_plot/build.py
 # Default imports
 
 import pandas as pd
@@ -7,12 +8,10 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
+variable2 = 'SalePrice'
+variable1 = 'GrLivArea'
 
 # Write your code here
 def regression_plot(variable1, variable2):
     sns.jointplot(variable1, variable2, data=data, kind='reg')
-    return plt.show()
-
-
-
-
+    return #plt.show()
