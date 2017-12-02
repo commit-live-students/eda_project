701fd89df1123583126f9086315ce692ae8a1200
gitint <suraj@greyatom.com>
2017-12-02 10:10:00 +0000

    changes

--- q04_cor/build.py
+++ q04_cor/build.py
@@ -7,5 +7,9 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here
+def cor(data):
+    plt.figure(figsize=(15, 10))
+    sns.heatmap(data.corr())
+    return plt.show()
 
 
