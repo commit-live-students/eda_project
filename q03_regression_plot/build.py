547540e0f4b27f05d98342d3a78b16cbddb4182d
NeelkanthMehta <neilstrong2003@yahoo.com>
2017-12-26 16:45:33 +0000

    Done

--- q03_regression_plot/build.py
+++ q03_regression_plot/build.py
@@ -9,7 +9,6 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here
-
-
-
-
+def regression_plot(variable1, variable2):
+    sns.lmplot(variable1,variable2, data=data, fit_reg=True)
+    return None
