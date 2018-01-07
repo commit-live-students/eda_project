662cbb86a1ffb582a6d9175b74d94dbad9e12fbe
ms10031990 <ms10031990@yahoo.com>
2018-01-07 14:04:13 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -4,7 +4,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
+num_cols = ['LotArea', 'GarageArea', 'OpenPorchSF', 'SalePrice']
+facet = None
 
+def plot(num_cols):
+    for i in range(0, len(num_cols), 2):
+        if len(num_cols) > i+1:
+            plt.figure(figsize=(10,4))
+            plt.subplot(121)
+            sns.boxplot(facet, num_cols[i], data = data)
+            plt.subplot(122)
+            sns.boxplot(facet, num_cols[i+1], data = data)
+            plt.tight_layout()
+            plt.show()
 
-# Write your code here:
-
+    return plot
