38d47fc45d29852226aa873bae2fddc71c7d5719
iamsathishsatz <Sathishbabu.s.g@gmail.com>
2018-01-08 17:04:44 +0000

    Done

--- q03_regression_plot/build.py
+++ q03_regression_plot/build.py
@@ -9,7 +9,11 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here
+x,y = 'GrLivArea', 'SalePrice'
+def regression_plot(x,y):
+#sns.lmplot('GrLivArea', 'SalePrice', data, fit_reg=True)
+# Write your code here
+    sns.jointplot(x,y, data=data, kind='reg')
+    plt.show()
 
-
-
-
+# regression_plot(x,y)
