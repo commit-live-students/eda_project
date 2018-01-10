426fc140b58bc3652daeb4cc8780c856dd2700e5
chiragpujari <chg.p786@gmail.com>
2018-01-10 02:53:46 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -7,4 +7,15 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here:
-
+def plot(num_cols):
+    for i in range(0,len(num_cols),2):
+        if len(num_cols) > i+1:
+            plt.figure(figsize=(10,4))
+            plt.subplot(121)
+            sns.boxplot(data[num_cols[i]])
+            plt.subplot(122)            
+            sns.boxplot(data[num_cols[i+1]])
+            plt.tight_layout()
+            plt.show()
+        else:
+            sns.boxplot(data[num_cols[i]])
