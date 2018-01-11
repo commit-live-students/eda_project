d931d320d418d1ef3f004704d0aaf1c371aeb8b7
djocz <jocelynd789@gmail.com>
2018-01-11 05:44:32 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -5,3 +5,14 @@ import pandas as pd
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
+def plot(data):
+    plt.subplot(4,2,1)
+    sns.distplot(data['LotArea'])
+    plt.subplot(4,2,2)
+    sns.distplot(data['GarageArea'])
+    plt.subplot(4,2,3)
+    sns.distplot(data['OpenPorchSF'])
+    plt.subplot(4,2,4)
+    sns.distplot(data['SalePrice'])
+    plt.show()
+    return 
