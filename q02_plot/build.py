3d9bc0fe81b0887168b656f61dcda4a17d8745e8
cchopade <cchopade@gmail.com>
2018-02-04 08:47:09 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -5,6 +5,10 @@ import seaborn as sns
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
-
+def plot(num_cols):
+    for d in num_cols:
+        sns.boxplot(data.iloc[:,d])
+        plt.show()
 # Write your code here:
 
+# Write your code here:
