9d3b79384c6cbea1c67bb995da0ab81ac8f940b4
cchopade <cchopade@gmail.com>
2018-02-04 08:59:44 +0000

    Done

--- q04_cor/build.py
+++ q04_cor/build.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
-
+def cor(data):
+    correlation = data.corr()
+    sns.heatmap(correlation, vmin = -1, vmax = 1)
+    plt.show()
 
 # Write your code here
