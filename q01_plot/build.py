bc60c0d6c64bce02d4d9d462702b64c1f4dfcd44
axc888 <jha.ashish43@gmail.com>
2018-03-02 19:10:39 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -5,3 +5,18 @@ import pandas as pd
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
+num_cols = ['LotArea', 'GarageArea', 'OpenPorchSF', 'SalePrice']
+# Write your code here:
+def plot(num_cols):
+    for i in range(0,len(num_cols),2):
+        if len(num_cols) > i+1:
+            plt.figure(figsize=(10,4))
+            plt.subplot(121)
+            sns.distplot(data[num_cols[i]], hist=True, kde=True)
+            plt.subplot(122)
+            sns.distplot(data[num_cols[i+1]], hist=True, kde=True)
+            plt.tight_layout()
+            plt.show()
+
+        else:
+            sns.distplot(data[num_cols[i]], hist=True, kde=True)
