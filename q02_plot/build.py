846efd4317f3c07d73ff9f976a98379ea2773194
PBPatil <theprasadpatil@gmail.com>
2018-03-04 15:13:12 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -7,4 +7,14 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here:
+num_cols = ['LotArea','GarageArea','OpenPorchSF','SalePrice']
+def plot(num_cols):
+    cols = 2
 
+    number_of_rows = len(num_cols)-1/cols
+    plt.figure(figsize=(15,20))
+    for i in range(0,len(num_cols)):
+        plt.subplot(number_of_rows,cols,i+1)
+        sns.boxplot(data[num_cols[i]].dropna())
+    plt.tight_layout()
+    plt.show()
