0dbed9e5541c404d2a45ab982707216b8287cae1
viishnu712 <viishnu712@gmail.com>
2018-03-05 04:37:27 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -5,3 +5,9 @@ import pandas as pd
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
+def plot(cols):
+    a = []
+    for i in range(0,len(cols)):
+        a.append(sns.distplot(data[cols[i]], kde=False))
+        #plt.show()
+    return a
