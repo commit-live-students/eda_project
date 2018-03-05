9cac2e3512f2d255cda74c070d11a1eadfbf956b
PBPatil <theprasadpatil@gmail.com>
2018-03-05 04:34:09 +0000

    Done

--- q03_regression_plot/build.py
+++ q03_regression_plot/build.py
@@ -9,7 +9,10 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here
-
-
-
-
+variable1 = data.GarageArea
+variable2 = data.GrLivArea
+def regression_plot(variable1,variable2):
+    sns.regplot(variable1,data.SalePrice)
+    plt.show()
+    sns.regplot(variable2,data.SalePrice)
+    plt.show()
