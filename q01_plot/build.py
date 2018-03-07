d66bdcf20be9439462d0680f35ff1eb5efdab354
sohelahmadkhan <sohelahmadkhan000@gmail.com>
2018-03-07 13:55:12 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -5,3 +5,15 @@ import pandas as pd
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
+col = ['LotArea','GarageArea','OpenPorchSF','SalePrice']
+
+def plot(num_cols):
+    noc = num_cols
+    nor = int(len(col)/noc)
+    plt.figure(figsize = (5*noc,5*nor))
+    for i in range(0,len(col)):
+        plt.subplot(nor+1,noc,i+1)
+        sns.distplot(data[col[i]],kde= False)
+    plt.show()
+
+#print(plot(3))
