60b333fe3c1d478d0fc5fea9b2d6104008d88d59
Sharad30 <sharad30.ss@gmail.com>
2018-03-07 15:07:20 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -5,3 +5,12 @@ import pandas as pd
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
+def plot(num_cols):
+
+    num_of_rows = len(num_cols)/2
+    for i in range(0, len(num_cols)):
+
+        plt.subplot(num_of_rows+1, 2, i+1)
+        sns.distplot(data[num_cols[i]], bins = 20, kde = False)
+
+    plt.show()
