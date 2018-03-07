6917aeb5b8b54f7474e7b031eeb1e63a8cf047ff
shravanighatnekar <joshipoonam0303@gmail.com>
2018-03-07 18:31:53 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -7,4 +7,7 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here:
-
+def plot(num_cols):
+    num_cols = [data.LotArea, data.GarageArea, data.OpenPorchSF, data.SalePrice]
+    sns.boxplot(num_cols)
+    show.plot()
