32475153df1d9670ee62576e71cd9cb159432d30
kvin15 <vinayakkhamkar@gmail.com>
2018-03-09 18:11:58 +0000

    Done

--- q03_regression_plot/build.py
+++ q03_regression_plot/build.py
@@ -9,7 +9,8 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here
-
-
-
-
+def regression_plot(variable_1, variable_2):
+    sns.pairplot(df, x_vars=[variable_1, variable_2], y_vars=['SalePrice'], size=10, aspect=.8, kind="reg");
+    plt.tight_layout()
+    # plt.show()
+    return plt
