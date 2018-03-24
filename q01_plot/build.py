754451e37d01cf7d34edff3b04bfbcf6c96a152f
gupta1006 <guptagauttam@gmail.com>
2018-03-24 07:42:54 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -1,8 +1,10 @@
-# %load q01_plot/build.py
+# %load q02_plot/build.py
 # Default imports
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pandas as pd
+
+data = pd.read_csv('data/house_prices_multivariate.csv')
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 cols = data[['LotArea','GarageArea','OpenPorchSF','SalePrice']]
@@ -11,7 +13,7 @@ num_cols = len(cols.columns)
 def plot(num_cols):
     for a in range(1,4):
         for n in range(0,num_cols):
-            plt.subplot(2,num_cols,a)
+            plt.subplot(num_cols,1,a)
             sns.distplot(cols[cols.columns[n]])
             plt.show()
         return
--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot/build.py
 # Default imports
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,6 +6,14 @@ import seaborn as sns
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
+data = pd.read_csv('data/house_prices_multivariate.csv')
+cols = data[['LotArea','GarageArea','OpenPorchSF','SalePrice']]
+num_cols = len(cols.columns)
 
-# Write your code here:
-
+def plot(num_cols):
+    for a in range(1,4):
+        for n in range(0,num_cols):
+            plt.subplot(num_cols,1,a)
+            sns.boxplot(cols[cols.columns[n]])
+            plt.show()
+        return
