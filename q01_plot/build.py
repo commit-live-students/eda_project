ad53b41466beef767c0a382065d4dc8aef270ab8
siddhichandan <siddhichandan@gmail.com>
2018-05-02 12:24:43 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -5,3 +5,7 @@ import pandas as pd
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 # Write your code here :
+def plot(l_cols):
+    for col in l_cols:
+        data.hist(column=col)
+        plt.show()
