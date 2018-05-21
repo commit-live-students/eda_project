5b6cb577d6745f75e31dd16ac7ca466f942c32b8
amol120586 <amol.24365@gmail.com>
2018-05-21 06:52:05 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot/build.py
 # Default imports
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,4 +8,21 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here:
+num_cols = ['LotArea', 'GarageArea', 'OpenPorchSF', 'SalePrice']
+
+def plot(num_cols):
+    for i in range(0,len(num_cols),2):
+        if len(num_cols) > i+1:
+            plt.figure(figsize=(10,4))
+            plt.subplot(121)
+            sns.boxplot(data[num_cols[i]])
+            plt.subplot(122)            
+            sns.boxplot(data[num_cols[i+1]])
+            plt.tight_layout()
+            plt.show()
+
+        else:
+            sns.boxplot(dataframe[num_cols[i]])
+
+
 
