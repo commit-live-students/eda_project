423693cb23376b97a6adbfb67e4173b5e7544e47
bharadwajk9 <utkb.k9@gmail.com>
2018-06-04 16:30:27 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -1,7 +1,28 @@
+# %load q01_plot/build.py
+# Default imports
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
+num_cols = ['LotArea', 'GarageArea', 'OpenPorchSF','SalePrice']
+# Write your code here:
+def plot(num_cols):
+    for i in range(0, len(num_cols),2):
+        if (len(num_cols)>i+1):
+            plt.figure(figsize=(10,4))
+            plt.subplot(121)
+            sns.distplot(data[num_cols[i]],kde=false)
+            plt.subplot(122)
+            sns.distplot(data[num_cols[i+1]],kde=false)
+            plt.tight_layout()
+            plt.show()
+        else:
+            sns.distplot(data[num_cols[i]],kde=false)
+            
 
-# Write your code here :
+
+#plot(num_cols)
+
+
+
