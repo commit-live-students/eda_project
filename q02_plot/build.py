c6f81cec69b7ea1204cfa8ceb8ebabd767184a81
HardikaB <hardi.2607@gmail.com>
2018-06-05 18:01:03 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot/build.py
 # Default imports
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,4 +8,21 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here:
+
+def plot(num_cols):
+    facet = None
+    for i in range(0,len(num_cols),2):
+        if len(num_cols) > i+1:
+            plt.figure(figsize=(10,4))
+            plt.subplot(121)
+            sns.boxplot(facet, num_cols[i],data = data)
+            plt.subplot(122)            
+            sns.boxplot(facet, num_cols[i+1],data = data)
+            plt.tight_layout()
+            plt.show()
+
+        else:
+            sns.boxplot(facet, num_cols[i],data = data)
+
+
 
