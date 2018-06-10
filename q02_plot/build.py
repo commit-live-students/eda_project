f857c7aab00e8f834890b4f92cd18c46bcf7b71e
rushi0109 <rushi0109@gmail.com>
2018-06-10 15:30:37 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,10 +1,26 @@
+# %load q02_plot/build.py
 # Default imports
-import pandas as pd
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
+import pandas as pd
+
 import seaborn as sns
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
-
+cols = ['LotArea' ,'GarageArea', 'OpenPorchSF','SalePrice']
 # Write your code here:
+def plot(num_cols):
+    for i in range(0, len(num_cols), 2):
+        if(i < len(num_cols)):
+            plt.figure(figsize=(10,4))
+            plt.subplot(121)
+            sns.boxplot(None, num_cols[i], data=data)
+            plt.subplot(122)
+            sns.boxplot(None, num_cols[i+1], data=data)
+            plt.tight_layout()
+            plt.show()
+
+plot(['LotArea','GarageArea','OpenPorchSF','SalePrice'])
 
