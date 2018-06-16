533ce30610fd4deb82f777cb4f43a4195d2c9df1
naitik1995 <naitikgada1995@gmail.com>
2018-06-16 18:12:17 +0000

    Done

--- q03_regression_plot/build.py
+++ q03_regression_plot/build.py
@@ -1,14 +1,21 @@
+# %load q03_regression_plot/build.py
 # Default imports
 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
+plt.switch_backend('agg')
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
-
+liv = 'GrLivArea'
+sale = 'SalePrice'
 # Write your code here
+def regression_plot(liv,sale):
+    sns.jointplot(liv,sale,data=data,kind='reg')
+    plt.show()
+regression_plot(liv,sale)
+
 
 
 
