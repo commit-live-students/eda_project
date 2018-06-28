27ddaac7c671988e05c963356c570631466ef036
vnylp <vinitdhamale.we@gmail.com>
2018-06-28 16:19:01 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -1,7 +1,22 @@
+# %load q01_plot/build.py
+# Default imports
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-
+plt.switch_backend('agg')
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
-# Write your code here :
+# Write your code here:
+def plot(a):
+    data.iloc[:,a[0]:a[0]+1].hist(bins=50)
+    data.iloc[:,a[1]:a[1]+1].hist(bins=50)
+    data.iloc[:,a[2]:a[2]+1].hist(bins=50)
+    data.iloc[:,a[3]:a[3]+1].hist(bins=50)
+    return plt.show()
+
+
+a=[1,25,27,34]
+plot(a)
+
+
+
