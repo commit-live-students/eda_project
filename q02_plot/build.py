4701fb690af32a07bc49c06e0332056e8f2946c0
vnylp <vinitdhamale.we@gmail.com>
2018-06-28 16:48:45 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,10 +1,21 @@
+# %load q02_plot/build.py
 # Default imports
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+plt.switch_backend('agg')
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here:
+def plot(a):
+    data.iloc[:,a[0]:a[0]+1].boxplot()
+    data.iloc[:,a[1]:a[1]+1].boxplot()
+    data.iloc[:,a[2]:a[2]+1].boxplot()
+    data.iloc[:,a[3]:a[3]+1].boxplot()
+    return plt.show()
+
+a=[1,25,27,34]
+plot(a)
+
 
