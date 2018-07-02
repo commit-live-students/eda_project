ac547ca92afa9e3de0236344d1ceb7659e631d9d
siddesh001 <siddesh.001@gmail.com>
2018-07-02 09:35:00 +0000

    Done

--- q04_cor/build.py
+++ q04_cor/build.py
@@ -1,3 +1,4 @@
+# %load q04_cor/build.py
 # Default imports
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,3 +8,11 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your code here
+def cor(data):
+    fig=plt.figure()
+    sns.heatmap(data.corr())
+    return fig
+
+    
+
+
