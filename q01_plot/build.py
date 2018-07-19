46ad1470ac55d5e293871832f3fcbe48c5a6fdef
jvdhalani <javedhalani@gmail.com>
2018-07-19 02:54:34 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -1,8 +1,23 @@
+# %load q01_plot/build.py
+# Default imports
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+#%matplotlib inline
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
-plt.switch_backend('agg')
 
-# Write your code here :
+
+# Write your code here:
+def plot(num_cols):
+    for i in range(0, len(num_cols), 2):
+        if(i < len(num_cols)):
+            plt.figure(figsize=(15,8))
+            plt.subplot(121)
+            sns.distplot(data[num_cols[i]],kde=True)
+            plt.subplot(122)
+            sns.distplot(data[num_cols[i+1]],kde=True)
+            plt.show()
+
