2480b1c965080a8839d70113b2e875dc1d391488
rajeshbrid <rajesh_brid@hotmail.com>
2018-09-18 15:04:36 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -1,8 +1,31 @@
+# %load q01_plot/build.py
+# Default imports
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
-plt.switch_backend('agg')
 
-# Write your code here :
+# Write your code here:
+def plot(num_cols = ['LotArea', 'GarageArea', 'OpenPorchSF', 'SalePrice']):
+    
+    for i in range(0,len(num_cols),2):
+        
+        if len(num_cols) > i+1:
+            plt.figure(figsize=(10,4))
+            plt.subplot(121)
+            sns.distplot(data[num_cols[i]], hist=True, kde=True)
+            plt.subplot(122)            
+            sns.distplot(data[num_cols[i+1]], hist=True, kde=True)
+            plt.tight_layout()
+            plt.show()
+
+        else:
+            sns.distplot(data[num_cols[i]], hist=True, kde=True)
+            
+    return
+
+plot(num_cols = ['LotArea', 'GarageArea', 'OpenPorchSF', 'SalePrice'])
+
+
+
--- q03_regression_plot/build.py
+++ q03_regression_plot/build.py
@@ -1,15 +1,26 @@
+# %load q03_regression_plot/build.py
 # Default imports
 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
-data = pd.read_csv('data/house_prices_multivariate.csv')
-plt.switch_backend('agg')
-
+df = pd.read_csv('data/house_prices_multivariate.csv')
 
 # Write your code here
+
+def regression_plot(var1=['SalePrice'],var2=['GrLivArea']):
+    #sns.regplot(x=data['SalePrice'], y=data['GrLivArea']) 
+    sns.lmplot(x='SalePrice',y='GrLivArea',data=df,size=12)
+
+    return
+
+#regression_plot(var1=['SalePrice'],var2=['GrLivArea'])
+
+#sns.plt.show() 
+
+
+
 
 
 
