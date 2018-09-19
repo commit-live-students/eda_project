fc810d9b129c70022c5d7e324037ef8d1682badb
rianboy18 <40799554+rianboy18@users.noreply.github.com>
2018-09-19 11:46:20 +0530

    Update build.py

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -10,19 +10,7 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 num_cols=list(data)
 
 def plot(num_cols):
-
-#     for i in range(0,len(num_cols),2):
-#         if len(num_cols) > i+1:
-#             plt.figure(figsize=(10,4))
-#             plt.subplot(121)
-#             sns.distplot(data[num_cols[i]], kde=False)
-#             plt.subplot(122)
-#             sns.distplot(data[num_cols[i+1]], kde=False)
-#             plt.tight_layout()
-#             plt.show()
-
-#         else:
-#             sns.distplot(data[num_cols], kde=False)  
+ 
     f, axes = plt.subplots(2, 2, figsize=(13,13), sharex=False)
     sns.distplot( data['LotArea'] , color='skyblue', ax=axes[0, 0])
     sns.distplot( data['GarageArea'] , color='olive', ax=axes[0, 1])
