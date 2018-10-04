fa5c8cce23d2a7d9e30bbab61f1df053162dc5e5
Tusharshah2006 <tusharshah2006@gmail.com>
2018-10-04 18:32:11 +0000

    Done

--- q04_cor/build.py
+++ q04_cor/build.py
@@ -1,3 +1,4 @@
+# %load q04_cor/build.py
 # Default imports
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,5 +7,12 @@ import seaborn as sns
 data = pd.read_csv('data/house_prices_multivariate.csv')
 plt.switch_backend('agg')
 
-
 # Write your code here
+def cor(df):
+    plt.figure(figsize=(12,8))
+    plot = sns.heatmap(df.corr(), annot=True, cmap='viridis')
+    return plot
+
+cor(data)
+
+
