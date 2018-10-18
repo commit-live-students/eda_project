376e75acf32e96313e00d4d23ac738d99f88121b
ShardulJ <Shardul2607@gmail.com>
2018-10-18 05:59:15 +0000

    Done

--- q04_cor/build.py
+++ q04_cor/build.py
@@ -1,10 +1,18 @@
+# %load q04_cor/build.py
 # Default imports
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
-plt.switch_backend('agg')
+
+def cor(data):
+    corr = data.corr()
+    sns.set()
+    ax = sns.heatmap(corr)
+    return ax
 
 
-# Write your code here
+
+
+
