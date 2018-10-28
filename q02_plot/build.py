0d56902b7f472400009f19dcd368c025064060ed
y0ge5h <c.9.yogesh@gmail.com>
2018-10-28 06:34:33 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,11 +1,15 @@
+# %load q02_plot/build.py
 # Default imports
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
-plt.switch_backend('agg')
 
 
 # Write your code here:
+def plot (num_cols):
+    print(sns)
+
+
 
