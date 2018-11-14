1a53312d492b0edf07bdecee6653b6db7772983e
sunilhariharan <sunilhariharan@gmail.com>
2018-11-14 14:54:12 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -1,8 +1,21 @@
+# %load q01_plot/build.py
+# Default imports
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-
+import numpy as np
 data = pd.read_csv('data/house_prices_multivariate.csv')
-plt.switch_backend('agg')
 
-# Write your code here :
+# Write your code here:
+def plot(num_cols):
+    num_cols=[data['LotArea'],data['GarageArea'],data['OpenPorchSF'],data['SalePrice']]
+    Plot=plt.hist(num_cols, bins=50, alpha=0.5, label=['LotArea','GarageArea','OpenPorchSF','SalePrice'])
+
+    plt.legend(loc='upper right')
+    
+    
+    return Plot
+    
+    
+
+
