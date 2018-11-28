5e0838892ab6d150f0406aa875d5a82a9f559375
amagarwal-18 <amitagarwal.skit@gmail.com>
2018-11-28 05:06:42 +0000

    Done

--- q01_plot/build.py
+++ q01_plot/build.py
@@ -1,8 +1,17 @@
+# %load q01_plot/build.py
+# Default imports
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
-plt.switch_backend('agg')
 
-# Write your code here :
+plt.switch_backend('agg')
+def plot(num_cols):
+    for col in num_cols:
+        sns.distplot(data[col], kde=True)
+        plt.show()
+num_cols = ['LotArea', 'GarageArea', 'OpenPorchSF', 'SalePrice']
+plot(num_cols)
+
+
