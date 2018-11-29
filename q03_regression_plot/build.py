b8deb3bf5399437f9280285fe53abb31c8553700
akashhchatterjee <akashh.chatterjee@gmail.com>
2018-11-29 18:10:37 +0000

    Done

--- q03_regression_plot/build.py
+++ q03_regression_plot/build.py
@@ -1,16 +1,18 @@
+# %load q03_regression_plot/build.py
 # Default imports
 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-
-data = pd.read_csv('data/house_prices_multivariate.csv')
 plt.switch_backend('agg')
 
+data = pd.read_csv('data/house_prices_multivariate.csv')
+x = data['GrLivArea']
+y = data['SalePrice']
 
 # Write your code here
-
-
-
+def regression_plot(x,y):
+    sns.lmplot('GrLivArea', 'SalePrice', data=data, fit_reg=True)
+    
+regression_plot(x,y)
 
