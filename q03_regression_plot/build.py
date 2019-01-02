83530a893de33a0d7a0e9aa506788a70777fca14
paragDC <paragchache06@gmail.com>
2019-01-02 12:52:41 +0000

    Done

--- q03_regression_plot/build.py
+++ q03_regression_plot/build.py
@@ -1,3 +1,4 @@
+# %load q03_regression_plot/build.py
 # Default imports
 
 import pandas as pd
@@ -6,10 +7,15 @@ import matplotlib.pyplot as plt
 
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
-plt.switch_backend('agg')
 
 
 # Write your code here
+def regression_plot(variable1,variable2):
+    x = df[variable1]
+    y = df[variable2]
+    print(x,y)
+
+
 
 
 
