19e4ad449e71e2ba7c82e8f02c4edb3353ad524e
Jas-simran <simranbright42@gmail.com>
2019-01-04 08:42:51 +0000

    Done

--- q03_regression_plot/build.py
+++ q03_regression_plot/build.py
@@ -1,3 +1,4 @@
+# %load q03_regression_plot/build.py
 # Default imports
 
 import pandas as pd
@@ -6,11 +7,25 @@ import matplotlib.pyplot as plt
 
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
-plt.switch_backend('agg')
 
 
 # Write your code here
+def regression_plot(variable1, variable2):
+    
+    return sns.regplot(data[variable1], data[variable2])
 
+
+
+va1 = 'GrLivArea'
+va2 = 'SalePrice'
+
+
+
+
+
+
+
+regression_plot(va1,va2)
 
 
 
