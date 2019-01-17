fe1a14e22bb4a377ebe8fa6ff5939f235f32334a
akshaylakade <Aakshaylakade@gmail.com>
2019-01-17 15:25:55 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,11 +1,26 @@
+# %load q02_plot/build.py
 # Default imports
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
-plt.switch_backend('agg')
 
 
 # Write your code here:
+def plot(num_cols):
+    for i in range(0,len(num_cols), 2):
+        if len(num_cols) > i+1:
+            plt.figure(figsize=(10,4))
+            plt.subplot(121)
+            sns.boxplot(data[num_cols[i]])
+            plt.subplot(122)
+            sns.boxplot(data[num_cols[i+1]])
+            plt.tight_layout()
+            # plt.show()
+        else:
+            sns.boxtplot(data[num_cols[i]])
+    return None
+
+
 
